ce0291387d0062828d36d68a8b937fc9bee0e5f6
phuctranngh <phuctran.ngh@gmail.com>
2024-11-07 13:50:04 +0700

    enhance build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import commonjs from "vite-plugin-commonjs";
+import { defineConfig } from "vite";
+import compressPlugin from "vite-plugin-compress";
 import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
+  plugins: [react(), eslintPlugin(), compressPlugin()],
   build: {
     sourcemap: true,
     chunkSizeWarningLimit: 1500,
@@ -15,20 +15,18 @@ export default defineConfig({
       external: ["oniguruma-to-js"],
       output: {
         manualChunks: {
-          vendor: [
-            "@chakra-ui/react",
-            "@emotion/react",
-            "@emotion/styled",
-            "react",
-            "react-dom",
-            "reactjs-tiptap-editor"
-          ]
+          chakra: ["@chakra-ui/react", "@emotion/react", "@emotion/styled"],
+          react: ["react", "react-dom"],
+          tiptap: ["reactjs-tiptap-editor"],
+          utils: ["lodash", "dayjs"],
+          redux: ["@reduxjs/toolkit", "react-redux", "redux-persist"]
         }
       }
     }
   },
   optimizeDeps: {
-    include: ["react", "react-dom"]
+    include: ["react", "react-dom"],
+    exclude: ["@chakra-ui/react", "reactjs-tiptap-editor"]
   },
   server: {
     port: 5000
